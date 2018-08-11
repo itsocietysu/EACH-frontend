648318e9e50840b9d444686144b914fc9032e511
Michail Nakhatovich <mish_1998@mail.ru>
2018-08-11 19:51:36 +0300

    Fixed logout

--- app/containers/LogoutButton/index.js
+++ app/containers/LogoutButton/index.js
@@ -5,24 +5,24 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 
 import { withRouter } from 'react-router-dom';
 import { setSession, setLogined } from 'cookieManager';
 
 import Button from 'components/Button';
+import { userdataGot } from 'containers/App/actions';
 import messages from './messages';
 
 class LogoutButton extends React.Component {
-  onLogout() {
-    setLogined(false);
-    setSession('');
-  }
   render() {
     return (
       <Button
         onClick={() => {
-          this.onLogout();
+          this.props.onLogout();
           this.props.history.push(`/`);
         }}
         children={<FormattedMessage {...messages.logout} />}
@@ -31,4 +31,26 @@ class LogoutButton extends React.Component {
   }
 }
 
-export default withRouter(LogoutButton);
+LogoutButton.propTypes = {
+  onLogout: PropTypes.func,
+};
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onLogout: () => {
+      setLogined(false);
+      setSession('');
+      dispatch(userdataGot({ name: '', accessType: 'user' }));
+    },
+  };
+}
+
+const withConnect = connect(
+  null,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  withRouter,
+)(LogoutButton);

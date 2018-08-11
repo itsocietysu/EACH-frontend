f9746235609676f42839916f1a7e269f1a363376
Michail Nakhatovich <mish_1998@mail.ru>
2018-08-11 20:50:16 +0300

    Fixed scopes in configs
    - now scopes are sent

--- app/containers/AuthPage/configs.js
+++ app/containers/AuthPage/configs.js
@@ -2,7 +2,7 @@ const configs = {
   oauth2RedirectUrl: '/auth',
   clientId: 'Gu2SCEBUwQV3TSlNIu8uMzvKRMYuGP5ePh044jGErO6O9RR0',
   clientSecret: '',
-  scopes: 'email',
+  scopes: ['email'],
   authorizationUrl: 'http://each.itsociety.su:5000/oauth2/authorize',
   tokenUrl: 'http://each.itsociety.su:5000/oauth2/token',
 };

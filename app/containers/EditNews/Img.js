ec3398a87ae8572954f0d1d446da1f72eab4bac6
Michail Nakhatovich <mish_1998@mail.ru>
2018-08-16 22:30:06 +0300

    Deleted unnecessary files

--- app/containers/EditNews/Img.js
+++
@@ -1,9 +0,0 @@
-import NormalImg from 'components/Img';
-import styled from 'styled-components';
-
-const Img = styled(NormalImg)`
-  max-width: 37em;
-  max-height: 300px;
-`;
-
-export default Img;

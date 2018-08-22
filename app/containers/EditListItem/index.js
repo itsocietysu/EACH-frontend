910e1b41852ac0b4ce519b84c998927a9e77bb2e
Michail Nakhatovich <mish_1998@mail.ru>
2018-08-22 19:37:19 +0300

    Changed item of edit list

--- app/containers/EditListItem/index.js
+++ app/containers/EditListItem/index.js
@@ -11,8 +11,11 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 
+import { getLocale } from 'cookieManager';
+
 import ListItem from 'components/ListItem';
 import H2 from 'components/H2';
+import H3 from 'components/H3';
 import P from 'components/P';
 import PopupEl from 'containers/EditForm';
 import Button from 'components/Button';
@@ -24,6 +27,8 @@ import Nav from 'containers/LinkList/Nav';
 import MsgBox from 'components/MsgBox';
 import { deleteFeedData, deleteMuseumData } from 'containers/EditPage/actions';
 import messages from './messages';
+
+import { DEFAULT_LOCALE } from '../../i18n';
 
 const imgStyle = {
   width: '3%',
@@ -69,7 +74,6 @@ export class EditListItem extends React.PureComponent {
     const { item } = this.props.item;
     if (this.props.Museum) {
       item.title = item.name;
-      item.text = item.desc;
       item.priority = '0';
     }
     const content = (
@@ -112,11 +116,12 @@ export class EditListItem extends React.PureComponent {
             <Img src={item.image} alt={`${this.state.content}-${item.eid}`} />
           </DivLeft>
           <DivRight>
-            <H2>{item.title}</H2>
+            <H2>{item.title[getLocale() || DEFAULT_LOCALE]}</H2>
           </DivRight>
         </div>
         <div>
-          <P>{item.text}</P>
+          <H3>{item.desc[getLocale() || DEFAULT_LOCALE]}</H3>
+          <P>{this.props.Feed && item.text[getLocale() || DEFAULT_LOCALE]}</P>
         </div>
       </Wrapper>
     );

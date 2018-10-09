9e9aa476bdcdad5f9d90b9c6b34e267e9c62e7fa
Michail Nakhatovich <mish_1998@mail.ru>
2018-10-09 18:40:30 +0300

    Deleted old form and list for editing

--- app/containers/EditForm/Img.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import NormalImg from '../../components/Img';
-
-const Img = styled(NormalImg)`
-  max-width: 37em;
-  max-height: 300px;
-`;
-
-export default Img;
